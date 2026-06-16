aeed85118cf6c1a82685febc7951812a9cd53ea3
prince mahendrakumar prajapati <2401030630043@silveroakuni.ac.in>
2026-06-17 01:15:24 +0530

    Update supabase-public.server.ts

--- src/lib/supabase-public.server.ts
+++ src/lib/supabase-public.server.ts
@@ -2,15 +2,15 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/integrations/supabase/types";
 
 export function publicClient() {
-  console.log("SERVER ENV CHECK");
   console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
   console.log(
     "SUPABASE_PUBLISHABLE_KEY =",
     process.env.SUPABASE_PUBLISHABLE_KEY
   );
 
-  throw new Error("STOP HERE");
+  throw new Error(
+    `URL=${!!process.env.SUPABASE_URL} KEY=${!!process.env.SUPABASE_PUBLISHABLE_KEY}`
+  );
 
   return createClient<Database>("x", "x");
 }
-
